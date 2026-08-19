291a9dd2105a18c1192c3d56ad61e1552087c865
Yousr Ahmed <yousrelghreeb277@gmail.com>
2026-08-19 20:12:00 +0300

    Stock Portfolio Tracker
    project 2 in CodeAlpha

---
+++ stock.py
@@ -0,0 +1,25 @@
+# 1. dictionery to store stock data
+stock_data = {
+    "AAPL": 250,
+    "GOOGL": 1800,
+    "MSFT": 300,
+    "AMZN": 3300,
+    "TSLA": 700,
+    "NVDA": 500,
+    "DOW JONES": 860
+}
+# 2. user input to get stock symbol
+stock_symbol = input("Enter the stock symbol(e.g., AAPL): ").upper()
+if stock_symbol in stock_data:
+    quantity = int(input("Enter the quantity of shares: "))
+    # 3. calculate total value of the stock
+    price = stock_data[stock_symbol]
+    total_value = quantity * price
+    print(f"Total value of {quantity} shares of {stock_symbol}: ${total_value}")
+
+    #save the stock data to a txt file
+    with open("stock_data.txt", "a") as file:
+      file.write(f"{stock_symbol}: {quantity} shares at ${price} each, Total Value: ${total_value}\n")
+    print("Stock data saved to stock_data.txt")
+else:
+    print("Stock symbol not found in the data.")
