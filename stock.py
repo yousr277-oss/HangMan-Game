430237c48322e184e0f29d053dba623b95388402
Yousr Ahmed <yousrelghreeb277@gmail.com>
2026-08-19 20:21:54 +0300

    Enhance user interaction with welcome and farewell messages
    Added welcome and farewell messages to user interaction.

--- stock.py
+++ stock.py
@@ -8,6 +8,9 @@ stock_data = {
     "NVDA": 500,
     "DOW JONES": 860
 }
+
+print ("Welcome\n Happy to assist you \n How can I help you?")
+
 # 2. user input to get stock symbol
 stock_symbol = input("Enter the stock symbol(e.g., AAPL): ").upper()
 if stock_symbol in stock_data:
@@ -17,6 +20,7 @@ if stock_symbol in stock_data:
     total_value = quantity * price
     print(f"Total value of {quantity} shares of {stock_symbol}: ${total_value}")
 
+print ("Come Back Soon!")
     #save the stock data to a txt file
     with open("stock_data.txt", "a") as file:
       file.write(f"{stock_symbol}: {quantity} shares at ${price} each, Total Value: ${total_value}\n")
